70db167b80ea75ab9abb2fbcb325b1ea491e7e89
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-09-12 04:08:45 +0200

    Display validation errors on the edit form

--- src/app/contacts/module.js
+++ src/app/contacts/module.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
+import angularMessages from 'angular-messages';
 import angularResource from 'angular-resource';
 import contactResource from './contact_resource';
 import states from './states';
 import uiRouter from 'angular-ui-router';
 
 export default angular.module('app.contacts', [
+  angularMessages,
   angularResource,
   uiRouter
 ])
