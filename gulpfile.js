6e2634d8203167db9f437f86a4ade90c2171acbc
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-09-13 11:23:10 +0200

    Add gulp `webpack:build-production` task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const gutil = require('gulp-util');
 const path = require('path');
 const webpack = require('webpack');
 
+const webpackConfig = require('./webpack.config.js');
+
 gulp.task('lint', () => {
   const eslint = require('gulp-eslint');
   const friendlyFormatter = require('eslint-friendly-formatter');
@@ -56,7 +58,29 @@ gulp.task('tdd', (done) => {
 });
 
 gulp.task('webpack:build', (done) => {
-  const config = require('./webpack.config.js');
+  const config = Object.create(webpackConfig);
+
+  webpack(config, (err, stats) => {
+    if (err) {
+      throw new gutil.PluginError('webpack', err);
+    }
+
+    gutil.log('[webpack]', stats.toString({
+      colors: true
+    }));
+    done();
+  });
+});
+
+// TODO dry it
+
+gulp.task('webpack:build-production', (done) => {
+  const config = Object.create(webpackConfig);
+
+  config.plugins = config.plugins.concat(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  );
 
   webpack(config, (err, stats) => {
     if (err) {
