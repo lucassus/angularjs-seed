2de0342026dc25a6eba34c8d94ae3dc15fc0d69c
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-09-13 17:09:09 +0200

    Tune ui router configuration, setup 404 state

--- src/app.js
+++ src/app.js
@@ -2,9 +2,37 @@ import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import './style.scss';
 import * as modules from './modules';
 import angular from 'angular';
+import template404 from './app/404.html';
+import uiRouter from 'angular-ui-router';
+
+function router($urlMatcherFactoryProvider, $urlRouterProvider) {
+  $urlMatcherFactoryProvider.strictMode(true);
+  $urlRouterProvider.when('', '/');
+}
+
+function notFoundState($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('404', {
+      template: template404
+    });
+
+  $urlRouterProvider.otherwise(($injector) => {
+    $injector.get('$state').go('404');
+  });
+}
 
 export default angular.module('app', [
+  uiRouter,
+
   modules.about.name,
   modules.contacts.name,
   modules.home.name,
-]);
+])
+  .config(router)
+  .config(notFoundState)
+  .run(($log, $rootScope, $state) => {
+    $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
+      $log.error('$stateChangeError', error);
+      $state.go('404');
+    });
+  });
--- src/app/contacts/states/list/state.js
+++ src/app/contacts/states/list/state.js
@@ -13,5 +13,6 @@ export default {
     }
   },
 
-  template
+  template,
+  url: '/'
 };
