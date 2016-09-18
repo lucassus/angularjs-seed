38018fd69e07c1db91dde3d323ca36efd4118841
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-09-18 22:55:13 +0200

    Optimize build

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,11 +1,22 @@
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const webpack = require('webpack');
 const _ = require('lodash');
 
 const config = require('./webpack.config.js');
 
 const plugins = config.plugins.concat(
+  new ProgressBarPlugin({
+    format: ':msg [:bar] :elapsed[s]'
+  }),
   new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin()
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    mangle: true,
+    comments: false
+  }),
+  new webpack.optimize.OccurenceOrderPlugin(true)
 );
 
 module.exports = _.extend({}, config, {
