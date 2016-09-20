4858d9c72c4795605c6b1a32850c79458b485c63
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-09-20 18:11:51 +0200

    Complete e2e spec for the home page

--- e2e/home_page_spec.js
+++ e2e/home_page_spec.js
@@ -1,8 +1,24 @@
 describe('Home page', () => {
 
-  it('should have a title', () => {
+  it('has a title', () => {
     browser.get('/');
     expect(browser.getTitle()).toEqual('Angular webpack seed');
+  });
+
+  it('has a page title', () => {
+    expect(element(by.css('.page-header h2'))
+      .getText()).toEqual('Hello World!');
+  });
+
+  describe('click on `Click me!` button', () => {
+
+    it('displays an alert', () => {
+      element(by.partialButtonText('Click me!')).click();
+
+      const alert = browser.switchTo().alert();
+      expect(alert.getText()).toEqual('Hello World!');
+    });
+
   });
 
 });
