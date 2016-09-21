745cf6061ca9c80b9c0059e4466ca1ed58f0049e
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-09-21 11:18:05 +0200

    Taking screenshots and browser logs for failed e2e scenarios

--- protractor.config.js
+++ protractor.config.js
@@ -1,3 +1,5 @@
+const utils = require('./e2e/utils');
+
 exports.config = {
   directConnect: true,
   chromeDriver: 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.22',
@@ -8,5 +10,31 @@ exports.config = {
   baseUrl: 'http://localhost:8080',
 
   framework: 'jasmine',
-  specs: ['e2e/**/*_spec.js']
+  specs: ['e2e/**/*_spec.js'],
+
+  onPrepare() {
+    /* global browser */
+    /* global jasmine */
+
+    // Cleanup artifacts dir
+    utils.cleanupArtifacts();
+
+    // Collect screenshots for failed scenarios
+    jasmine.getEnv().addReporter({
+      specDone(result) {
+        if (result.status === 'failed') {
+          utils.takeScreenshot(result.fullName);
+        }
+      }
+    });
+
+    // Write browser's logs
+    jasmine.getEnv().addReporter({
+      specDone(result) {
+        browser.manage().logs().get('browser').then((browserLog) => {
+          utils.writeBrowserLog(result.fullName, browserLog);
+        });
+      }
+    });
+  }
 };
