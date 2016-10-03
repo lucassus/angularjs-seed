cc1dae0b94ae50974b5b482b3d6a8e4ce2813d2f
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-10-03 17:35:56 +0200

    Implement `appUniqueEmail` validator

--- src/app/commons/components/config.js
+++ src/app/commons/components/config.js
@@ -1,8 +1,12 @@
 import footerComponent from './footer/component';
 import navigationComponent from './navigation/component';
+import uniqueEmailDirective from './unique_email/directive';
 
 export default function($compileProvider) {
   'ngInject';
-  $compileProvider.component('appFooter', footerComponent);
-  $compileProvider.component('appNavigation', navigationComponent);
+
+  $compileProvider
+    .component('appFooter', footerComponent)
+    .component('appNavigation', navigationComponent)
+    .directive('appUniqueEmail', uniqueEmailDirective);
 }
