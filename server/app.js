4ee2a30d48f8348bceb1ad3aca40db5c204b27e9
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-10-18 19:38:26 +0200

    Add express http logging

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(require('body-parser').json());
+app.use(morgan('short'));
 
 if (process.env.NODE_ENV !== 'production') {
   app.use('/api/seed', require('./api/seed'));
