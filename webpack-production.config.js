065fa274fc79a223510797b504634da43b16c0f0
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-11-12 13:44:07 +0100

    Bring back CommonsChunkPlugin for the production release

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,9 +1,14 @@
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const path = require('path');
 const webpack = require('webpack');
 const _ = require('lodash');
 
 const config = require('./webpack.config.js');
+
+const CHUNK_FILENAME = '[name].[hash].js';
+
+// TODO bring back `new ExtractTextPlugin('style.[chunkhash].css')`
 
 const plugins = config.plugins.concat(
   new FaviconsWebpackPlugin({
@@ -11,6 +16,10 @@ const plugins = config.plugins.concat(
   }),
   new ProgressBarPlugin({
     format: ':msg [:bar] :percent'
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    fileName: CHUNK_FILENAME
   }),
   new webpack.optimize.DedupePlugin(),
   new webpack.optimize.UglifyJsPlugin({
@@ -24,6 +33,27 @@ const plugins = config.plugins.concat(
 );
 
 module.exports = _.extend({}, config, {
+  entry: {
+    vendor: [
+      'jquery',
+      'lodash',
+      'angular',
+      'angular-animate',
+      'angular-messages',
+      'angular-resource',
+      'angular-loading-bar',
+      'angular-toastr',
+      'angular-ui-router'
+    ],
+    app: ['./src/app.js']
+  },
+
+  output: {
+    path: path.resolve('build'),
+    filename: CHUNK_FILENAME,
+    chunkFilename: CHUNK_FILENAME
+  },
+
   plugins,
   devtool: 'source-map'
 });
