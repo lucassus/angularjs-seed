812032c45c2c57b2d3b5eecfaa5c3a4592b568ef
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-11-14 09:06:18 +0100

    Tuner webpack production config

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,38 +1,15 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const combineLoaders = require('webpack-combine-loaders');
 const path = require('path');
 const webpack = require('webpack');
-const _ = require('lodash');
 
-const config = require('./webpack.config.js');
-
+const BUILD_DIRECTORY = 'build';
 const CHUNK_FILENAME = '[name].[hash].js';
 
-// TODO bring back `new ExtractTextPlugin('style.[chunkhash].css')`
-
-const plugins = config.plugins.concat(
-  new FaviconsWebpackPlugin({
-    logo: './src/images/logo.png'
-  }),
-  new ProgressBarPlugin({
-    format: ':msg [:bar] :percent'
-  }),
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    fileName: CHUNK_FILENAME
-  }),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    },
-    mangle: true,
-    comments: false
-  }),
-  new webpack.optimize.OccurenceOrderPlugin(true)
-);
-
-module.exports = _.extend({}, config, {
+module.exports = {
   entry: {
     vendor: [
       'jquery',
@@ -49,11 +26,76 @@ module.exports = _.extend({}, config, {
   },
 
   output: {
-    path: path.resolve('build'),
+    path: path.resolve(BUILD_DIRECTORY),
     filename: CHUNK_FILENAME,
     chunkFilename: CHUNK_FILENAME
   },
 
-  plugins,
+  plugins: [
+    new CleanWebpackPlugin(BUILD_DIRECTORY, {
+      verbose: true
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './src/images/logo.png'
+    }),
+    new ProgressBarPlugin({
+      format: ':msg [:bar] :percent'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      fileName: CHUNK_FILENAME
+    }),
+    new ExtractTextPlugin('style.[hash].css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      mangle: true,
+      comments: false
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: combineLoaders([{
+        loader: 'ng-annotate'
+      }, {
+        loader: 'babel',
+        query: {
+          extends: path.join(__dirname, '.babelrc')
+        }
+      }])
+    }, {
+      test: /\.html$/,
+      loader: 'html'
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('style', 'css!sass')
+    }, {
+      test: /\.png$/,
+      loader: 'url?limit=100000'
+    }, {
+      test: /\.jpg$/,
+      loader: 'file'
+    }, {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=image/svg+xml'
+    }]
+  },
+
   devtool: 'source-map'
-});
+};
