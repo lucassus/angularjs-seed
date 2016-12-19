14b2b7b4cccf60fa8b69b0257b088b73dc91090b
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-12-19 16:25:36 +0100

    Simplify fast TDD script

--- client/fast-tdd.js
+++ client/fast-tdd.js
@@ -1,43 +1,34 @@
 #!/usr/bin/env node
 
-const _ = require('lodash');
-const fs = require('fs');
 const path = require('path');
-const rimraf = require('rimraf');
-
 const webpack = require('webpack');
 const KarmaServer = require('karma').Server;
 
-rimraf.sync('./client/build-test');
-
 const compiler = webpack(require('./webpack-test-fast.config'));
 
-process.stdout.write('Starting webpack');
-const timeout = setInterval(() => {
-  process.stdout.write('.');
-}, 500);
+let karmaStarted = false;
 
 const watcher = compiler.watch({
   aggregateTimeout: 300
-}, function(err, stats) {
+}, (err, stats) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  clearInterval(timeout);
-
-  process.stdout.write('\n');
   console.log(stats.toString({
     chunks: false,
     colors: true
   }));
 
-  const config = _.extend({}, {
-    configFile: path.join(__dirname, 'karma-fast.config.js')
-  }, { singleRun: false });
-  const server = new KarmaServer(config);
-  server.start();
+  if (!karmaStarted) {
+    const server = new KarmaServer({
+      configFile: path.join(__dirname, 'karma-fast.config.js')
+    });
+    server.start();
+
+    karmaStarted = true;
+  }
 });
 
 ['SIGINT', 'SIGTERM'].forEach((signal) => {
