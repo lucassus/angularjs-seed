bbf080e28209330b4440330e4569b13c4b4a29db
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-02-01 16:39:51 +0100

    Fix protractor setup

--- e2e/protractor.config.js
+++ e2e/protractor.config.js
@@ -2,7 +2,7 @@ const utils = require('./utils');
 
 exports.config = {
   directConnect: true,
-  chromeDriver: '../node_modules/webdriver-manager/selenium/chromedriver_2.26',
+  chromeDriver: '../node_modules/webdriver-manager/selenium/chromedriver_2.27',
   capabilities: {
     browserName: 'chrome'
   },
